dab18b026dd83450fc571584e329fa0a2a91f59a
D3stinn3 <paynedestinne@gmail.com>
2023-12-15 18:01:46 +0300

    Url Config Auth

--- ChurchAPI/urls.py
+++ ChurchAPI/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('eventapi/', include('eventapi.urls')),
-    path('accountapi/', include('accountapi.urls'))
+    path('accountapi/', include('accountapi.urls')),
+    path('api-token-auth', views.obtain_auth_token, name='authtoken')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
