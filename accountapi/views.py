c2ba63c415053d00a59dd9015f07caf0cdd7e260
D3stinn3 <paynedestinne@gmail.com>
2023-12-15 21:55:56 +0300

    Views 2

--- accountapi/views.py
+++ accountapi/views.py
@@ -13,15 +13,16 @@ from rest_framework.generics import GenericAPIView
 from accountapi.serializers import UserSerializer
 
 
-class UserLoginView(APIView):
+class UserLoginView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
         user = get_object_or_404(User, username=request.data['username'])
         if not user.check_password(request.data['password']):
             return Response({"Detail": "User Not Found"}, status=status.HTTP_404_NOT_FOUND)
         token, created = Token.objects.get_or_create(user=user)
-        serializer = UserSerializer(instance=user)
+        serializer = self.serializer_class(instance=user)
         if user:
             if user.is_active:
                 return Response({"message": "Success, BaseUser Logged In", "token": token.key, "user": serializer.data})
