639f2f4137308644e5d46d802f239767ce35209e
D3stinn3 <paynedestinne@gmail.com>
2023-12-15 18:29:24 +0300

    Settings

--- ChurchAPI/settings.py
+++ ChurchAPI/settings.py
@@ -156,8 +156,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-]
+    ]
 }
